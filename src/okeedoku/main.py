ce4a14710ff4b775ee2f9e847688f1351483a989
Isaac Jackson <mrisaacjackson@gmail.com>
2021-05-15 09:38:05 +1000

    added some models

--- src/okeedoku/main.py
+++ src/okeedoku/main.py
@@ -2,14 +2,27 @@ from typing import Annotated, Set, Tuple
 
 from pydantic import BaseModel, Field
 
+SIZE = 4
+
+
+class Coord(BaseModel):
+    x: int
+    y: int
+
 
 class Square(BaseModel):
-    possibles: Annotated[Set[int], 9] = Field({1, 2, 3, 4, 5, 6, 7, 8, 9})
+    possibles: Set[int] = Field(set(range(1, SIZE + 1)))
 
 
 class Game(BaseModel):
-    rows: Annotated[Tuple[Square], 9]
+    rows: Annotated[Tuple[Annotated[Tuple[Square], SIZE]], SIZE] = Field(
+            tuple(tuple(Square() for _ in range(SIZE)) for _ in range(SIZE)))
+    columns: Annotated[Tuple[Annotated[Tuple[Square], SIZE]], SIZE] = Field(
+            tuple(tuple(Square() for _ in range(SIZE)) for _ in range(SIZE)))
+    boxes: Annotated[Tuple[Annotated[Tuple[Square], SIZE]], SIZE] = Field(
+            tuple(tuple(Square() for _ in range(SIZE)) for _ in range(SIZE)))
 
 
 def main():
     print("Under construction")
+    print(f"{Game()}")
